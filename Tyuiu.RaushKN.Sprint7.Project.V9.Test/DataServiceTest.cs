a6923353561e3f9ad58ace5efd03de4b1038a42d
Кирилл Рауш <Tyuiu.RaushKN@outlook.com>
2024-12-26 20:50:56 +0500

    Beta 1.3

--- Tyuiu.RaushKN.Sprint7.Project.V9.Test/DataServiceTest.cs
+++ Tyuiu.RaushKN.Sprint7.Project.V9.Test/DataServiceTest.cs
@@ -5,30 +5,28 @@ namespace Tyuiu.RaushKN.Sprint7.Project.V9.Test
     [TestClass]
     public class DataServiceTest
     {
+        DataService ds = new DataService();
+
         [TestMethod]
-        public void Valid()
+        public void ValidFilmsData()
+        {
+            string path = $@"C:\Users\rausc\source\repos\Tyuiu.RaushKN.Sprint7\Tyuiu.RaushKN.Sprint7.Project.V9\bin\Debug\FilmsData.csv";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+
+
+        [TestMethod]
+        public void ValidGenreCount()
         {
             DataService ds = new DataService();
-            [TestMethod]
-            public void ValidFilmsData()
-            {
-                string path = $@"C:\Users\rausc\source\repos\Tyuiu.RaushKN.Sprint7\Tyuiu.RaushKN.Sprint7.Project.V9\bin\Debug\FilmsData.csv";
 
-                FileInfo fileInfo = new FileInfo(path);
-                bool fileExists = fileInfo.Exists;
-                bool wait = true;
-                Assert.AreEqual(wait, fileExists);
-            }
-
-
-            [TestMethod]
-            public void ValidGenreCount()
-            {
-                DataService ds = new DataService();
-
-                int[] wait = { 2, 1, 1, 2, 2, 0, 3 };
-                int[] result = ds.GetGenreStatistics();
-                CollectionAssert.Equals(wait, result);
-            }
+            int[] wait = { 2, 1, 1, 2, 2, 0, 3 };
+            int[] result = ds.GetGenreStatistics();
+            CollectionAssert.Equals(wait, result);
+        }
     }
 }
